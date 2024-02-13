02ec00fa5c99b90826398f2f129a113d8ca45215
Usmanc18 <usmanchoudhary2882@gmail.com>
2024-02-12 20:17:47 -0800

    Made it so notes save locally

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
+
+window.onload = function() {
+    loadNotesFromLocalStorage();
+};
+
+function loadNotesFromLocalStorage() {
+    const savedNotes = localStorage.getItem('notes');
+    if (savedNotes) {
+        const notesArray = JSON.parse(savedNotes);
+        notesArray.forEach(noteContent => {
+            createNoteElement(noteContent);
+        });
+    }
+}
+
 function sendNote() {
+    const inputElement = document.getElementById("input");
+    const noteContent = inputElement.value.trim();
+    if (noteContent !== '') {
+        createNoteElement(noteContent);
 
-const inputElement = document.getElementById("input")
-const noteContent = inputElement.value; 
- inputElement.value = "";
+        // Save the notes data to localStorage
+        saveNotesToLocalStorage();
+    }
+    inputElement.value = "";
+}
 
- let newNote = document.createElement("li")
- newNote.style = "height: 150px;";
- newNote.style.backgroundColor = getRandomColor();
- newNote.classList.add("container");
-newNote.innerHTML = noteContent;
-let allNotes = document.getElementById("notes");
-allNotes.appendChild(newNote);
-} 
+function saveNotesToLocalStorage() {
+    const notesContainer = document.getElementById('notes');
+    const notesArray = Array.from(notesContainer.children).map(note => note.textContent.trim());
+    localStorage.setItem('notes', JSON.stringify(notesArray));
+}
+
+function createNoteElement(noteContent) {
+    // Create a new note element
+    const newNote = document.createElement("li");
+    newNote.style = "height: 150px;";
+    newNote.style.backgroundColor = getRandomColor();
+    newNote.classList.add("container");
+    newNote.textContent = noteContent;
+
+    
+
+    // Append the new note to the list
+    const allNotes = document.getElementById("notes");
+    allNotes.appendChild(newNote);
+
+}
 
 function getRandomColor() {
     const COLORS = [
